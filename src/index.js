0c5d67c6458dc21ec1475414de5da024e9135ee6
William Halbert <willhalbert16@gmail.com>
2023-08-17 13:16:15 -0400

    hi

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
